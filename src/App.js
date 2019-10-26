cbdce28ce2f62235dcee3b432ec6651e945a635c
douglasgomes98 <douglasgomes.rr@gmail.com>
2019-10-26 16:19:56 -0400

    add routes and global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from './styles/global';
+import Routes from './routes';
 
 function App() {
-  return <h1>Hello Redux</h1>;
+  return (
+    <BrowserRouter>
+      <Routes />
+      <GlobalStyles />
+    </BrowserRouter>
+  );
 }
 
 export default App;
